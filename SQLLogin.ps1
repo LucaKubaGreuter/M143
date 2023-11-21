8cf5f1542369eb4e53d889f83037e3b9b0c8eb34
LucaKubaGreuter <luca.greuter@edu.tbz.ch>
2023-11-21 11:50:17 +0100

    ServerLoggins

---
+++ SQLLogin.ps1
@@ -0,0 +1,1 @@
+ssh ubuntu@3.228.34.74 -i "C:\Users\lucag\sql_rsa" -o ServerAliveInterval=30
---
+++ ServerLogin.ps1
@@ -0,0 +1,1 @@
+ssh kuba@192.168.105.128 -i "C:\Users\lucag\OneDrive - TBZ\TBZ\M143\id_rsa" -o ServerAliveInterval=30
